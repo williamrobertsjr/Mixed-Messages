4f7795c86148ee99099309bdd3b94da52714ba15
Billy Roberts <billy@billyroberts.co>
2023-08-13 20:37:13 -0400

    Added tracks to songs array and intial style rules

--- messages.js
+++ messages.js
@@ -1,13 +1,105 @@
 // set up array
-const quotes = [
+const songs = [
     {
         id: 1,
-        message: 'Test Message 1' 
+        track: "Everything in it's Right Place",
+        album: "Kid A"
 
     },
     {
         id: 2,
-        message: 'Test Message 2'
+        track: "Pyramid Song",
+        album: "Amensiac"
+    },
+    {
+        id: 3,
+        track: "Decks Dark",
+        album: "A Moon Shaped Pool"
+    },
+    {
+        id: 4,
+        track: "Airbag",
+        album: "Ok Computer"
+    },
+    {
+        id: 5,
+        track: "A Punch Up at a Wedding",
+        album: "Hail to the Thief"
+    },
+    {
+        id: 6,
+        track: "Planet Telex",
+        album: "The Bends"
+    },
+    {
+        id: 7,
+        track: "Paranoid Android",
+        album: "Ok Computer"
+    },
+    {
+        id: 8,
+        track: "Idioteque",
+        album: "Kid A"
+    },
+    {
+        id: 9,
+        track: "You and Whose Army?",
+        album: "Amnesiac"
+    },
+    {
+        id: 10,
+        track: "Codex",
+        album: "King of Limbs"
+    },
+    {
+        id: 11,
+        track: "Jigsaw Falling Into Place",
+        album: "In Rainbows"
+    },
+    {
+        id: 12,
+        track: "Weird Fishes/ Apeggi",
+        album: "In Rainbows"
+    },
+    {
+        id: 13,
+        track: "The National Anthem",
+        album: "Kid A"
+    },
+    {
+        id: 14,
+        track: "Karma Police",
+        album: "Ok Computer"
+    },
+    {
+        id: 15,
+        track: "Burn the Witch",
+        album: "A Moon Shaped Pool"
+    },
+    {
+        id: 16,
+        track: "Just",
+        album: "The Bends"
+    },
+    {
+        id: 17,
+        track: "The Bends",
+        album: "The Bends"
+    },
+    {
+        id: 18,
+        track: "Sit Down. Stand Up.",
+        album: "Hail to the Thief"
+    },
+    {
+        id: 19,
+        track: "Lotus Flower",
+        album: "King of Limbs"
+    },
+    {
+        id: 20,
+        track: "Packt Like Sardines in a Crushd Tin Box",
+        album: "Amnesiac"
     },
 ]
 
